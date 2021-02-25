6c5e6051ae4c806cc304c46c4e17cd8ba8f77849
andreyess <53977922+andreyess@users.noreply.github.com>
2021-02-25 08:03:18 +0300

    Homework 2. Variable rename

--- task1.py
+++ task1.py
@@ -1,8 +1,8 @@
 #!/bin/bash python
 
 # Input value which will be used as the max-value in range-generation
-max = int(input())
+max_value = int(input())
 
 # Print i**2 value for each non-negative integer less than max
-for i in range(max):
+for i in range(max_value):
     print(i**2)
